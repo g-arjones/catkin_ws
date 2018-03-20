efd0a586073b3a93af000fc2f2252ef313970d08
Gabriel Arjones <arjones@arjones.com>
2018-03-20 13:54:06 -0300

    initialize rosdep in init.bash

--- init.bash
+++ init.bash
@@ -7,6 +7,8 @@ export ROS_OS_OVERRIDE="ubuntu:16.04:xenial"
 
 mkdir -p src
 wstool init -j8 src mccr.rosinstall
+sudo rosdep init
+rosdep update
 rosdep install --from-paths src -i --rosdistro kinetic -y
 
 catkin init > /dev/null
